0e5a4e73d4931b68c6fd5e328c1bee0eff5212c8
IevaVis <ieva.visniauskiene@gmail.com>
2018-06-07 22:58:54 +0800

    javascript and images

---
+++ app.js
@@ -0,0 +1,72 @@
+let scores = [0, 0];
+let countScore = 0;
+let currentPlayer = 0;
+document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = '0'
+document.getElementById("score-1").textContent = '0'
+document.getElementById("current-0").textContent = '0'
+document.getElementById("current-1").textContent = '0'
+
+
+document.querySelector(".btn-roll").addEventListener('click', function (){
+	let dice = Math.floor(Math.random() * 6) + 1;
+	let diceDOM = document.querySelector(".dice");
+	diceDOM.style.display = "block";
+	diceDOM.src = "dice-" + dice + ".png"
+	if(dice > 1) {
+		countScore += dice
+		document.querySelector('#current-' + currentPlayer).textContent = countScore
+	} else {
+		nextPlayer();
+	}
+
+});
+
+document.querySelector(".btn-hold").addEventListener('click', function (){
+	scores[currentPlayer] += countScore;
+	document.querySelector('#score-' + currentPlayer).textContent = scores[currentPlayer];
+	nextPlayer();	
+});
+
+function nextPlayer() {
+		currentPlayer === 0 ? currentPlayer = 1 : currentPlayer = 0;
+		countScore = 0;
+
+		document.getElementById("current-0").textContent = '0';
+		document.getElementById("current-1").textContent = '0';
+		document.querySelector('.player-0-panel').classList.toggle("active");
+		document.querySelector('.player-1-panel').classList.toggle("active");
+		
+		document.querySelector('.dice').style.display = "none";
+
+}
+
+document.querySelector(".btn-new").addEventListener('click', function(){
+	location.reload();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
